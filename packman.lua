350856bb3c6f6d5b9aba8d3fc39b3b1256ab463c
Daarknes <daarknes.gaming@gmail.com>
2024-07-18 20:21:32 +0200

    argparse subparsers

--- packman.lua
+++ packman.lua
@@ -1,3 +1,5 @@
+local argparse = require("daarknes/core/argparse")
+
 -- repository url
 local baseUrl = "https://raw.githubusercontent.com/Daarknes/computercraft/main/"
 
@@ -17,35 +19,14 @@ local function update()
     else
         -- create temporary helper file to download new version
         local f = io.open("_packman_temp.lua", "w")
-        f.write(request.readAll())
+        f:write(request.readAll())
         f:close()
 
         shell.execute("move _packman_temp.lua " .. progName)
     end
 end
 
-local function handleArguments(args, options)
-	if args[1] == "_finalizeUpdate" then
-		finalizeUpdate()
-	elseif args[1] == "update" then
-		update()
-	elseif args[1] == "list" then
-		showList()
-	elseif args[1] == "install" and args[2] ~= nil then
-		if libraries[args[2]] then
-			installLib(args[2], options.f)
-		elseif programs[args[2]] then
-			installProgram(args[2], options.f)
-		else
-			print("\"" .. args[2] .. "\" is not a valid library or program. Use 'packman list' for a list of all available libraries and programs.")
-		end
-	elseif args[1] == "uninstall" and args[2] ~= nil then
-		uninstall(args[2])
-	else
-		showHelp()
-	end
-end
 
 
-local args, options = {...}
-handleArguments(args, options)
+local parser = argparse:Parser("packman", "Package Manager for Computercraft Programs")
+local parserUpdate = parser:AddSubParser("update", "The update subparser")
